17ff5714f49a93f83987c04751c9bf9ce71f97a8
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-01 23:39:28 +0300

    feat: add HomePageView with context data for homepage

--- homepage_user/views.py
+++ homepage_user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from core.models import SiteSettings
+from communications.models import Response, Invitation
+from companies.models import Vacancy
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'homepage_user/homepage.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = SiteSettings.objects.first()
+        context['logo'] = obj.logo
+        context['main_banner'] = obj.main_banner
+        context['total_responses'] = Response.objects.count()
+        context['total_invitations'] = Invitation.objects.count()
+        context['vacancies'] = Vacancy.objects.select_related('company', 'profession')[:7]
+        return context
