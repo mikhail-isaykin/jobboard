0dfdfe9fbd5e3e7be4c8b6173fd3836bb12e3ff6
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-05-01 16:04:26 +0300

    feat(communications): register Response in admin

--- communications/admin.py
+++ communications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Response
 
-# Register your models here.
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+    list_display = ('vacancy', 'resume', 'status', 'created_at')
+    search_fields = ('vacancy__title', 'resume__last_name')
+    list_filter = ('status', 'created_at')
